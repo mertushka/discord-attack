974592c0a9ee6b2aeccd6a3ec479fca269348fe0
Bicepsfako <34413473+rootschwer@users.noreply.github.com>
2019-11-03 01:44:16 +0300

    Better Proxies

--- Source/scraper.js
+++ Source/scraper.js
@@ -3,7 +3,7 @@ const request = require('request');
 var scrapeProxies = new Promise((resolve, reject) => {
     request({
         method: "GET",
-        url: "https://api.proxyscrape.com/?request=displayproxies&proxytype=http",
+        url: "https://api.proxyscrape.com/?request=displayproxies&proxytype=http&timeout=1000",
     }, (error, response, body) => {
         if (body) {
             return resolve(body.split("\n"));
